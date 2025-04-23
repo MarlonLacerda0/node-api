9844d55550937224cf01c3381af0f852915bf80d
Marlon Lacerda <marlonlac10@hotmail.com>
2025-04-22 21:58:49 -0300

    Novas mudanças

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,11 @@
-import express,{} from 'express';
+import express,{Express, Request, Response} from 'express';
 
-const app = express()
+const app: Express = express();
 
-app.get('/', (req, res) => {
+app.get('/', (req: Request, res: Response) => {
     res.send('Hello World!')
   })
+
+app.listen(3000, () => {
+  console.log(`App de exemplo esta rodando na porta ${3000}`)
+})
